d8e55dd707a9b2697aadb243098d33c25f0d2cfc
hinata <ryugamine020401@gmail.com>
2026-06-24 08:18:39 +0800

    Day7 用 codex 新增其他第一族金屬、入水會爆炸

--- behavior_pack/scripts/main.js
+++ behavior_pack/scripts/main.js
@@ -1,20 +1,73 @@
 import { world, system, EntityComponentTypes } from "@minecraft/server";
 
-const POTASSIUM_ITEM_ID = "chemistry_plus:potassium_metal";
-const POTASSIUM_BLOCK_ID = "chemistry_plus:potassium_metal_block";
+const ALKALI_METALS = [
+  {
+    name: "鋰",
+    itemId: "chemistry_plus:lithium_metal",
+    blockId: "chemistry_plus:lithium_metal_block",
+    explosionRadius: 2,
+    waterMessage: "鋰金屬掉入水中：產生輕微爆炸！"
+  },
+  {
+    name: "鈉",
+    itemId: "chemistry_plus:sodium_metal",
+    blockId: "chemistry_plus:sodium_metal_block",
+    explosionRadius: 4,
+    waterMessage: "鈉金屬掉入水中：發生爆炸！"
+  },
+  {
+    name: "鉀",
+    itemId: "chemistry_plus:potassium_metal",
+    blockId: "chemistry_plus:potassium_metal_block",
+    explosionRadius: 8,
+    waterMessage: "鉀金屬掉入水中：發生大爆炸！"
+  },
+  {
+    name: "銣",
+    itemId: "chemistry_plus:rubidium_metal",
+    blockId: "chemistry_plus:rubidium_metal_block",
+    explosionRadius: 10,
+    waterMessage: "銣金屬掉入水中：發生劇烈爆炸！"
+  },
+  {
+    name: "銫",
+    itemId: "chemistry_plus:cesium_metal",
+    blockId: "chemistry_plus:cesium_metal_block",
+    explosionRadius: 12,
+    waterMessage: "銫金屬掉入水中：發生超劇烈爆炸！"
+  },
+  {
+    name: "鍅",
+    itemId: "chemistry_plus:francium_metal",
+    blockId: "chemistry_plus:francium_metal_block",
+    explosionRadius: 14,
+    waterMessage: "鍅金屬掉入水中：發生毀滅性爆炸！"
+  }
+];
 
-const REACTION_TAG = "chemistry_plus_potassium_reacted";
-const EXPLOSION_RADIUS = 8;
+const METAL_BY_ITEM_ID = new Map(ALKALI_METALS.map((metal) => [metal.itemId, metal]));
+const METAL_BY_BLOCK_ID = new Map(ALKALI_METALS.map((metal) => [metal.blockId, metal]));
+
+const REACTION_TAG = "chemistry_plus_alkali_metal_reacted";
 const CHECK_INTERVAL_TICKS = 5;
 
 world.afterEvents.playerPlaceBlock.subscribe((event) => {
   const player = event.player;
   const block = event.block;
+  const metal = METAL_BY_BLOCK_ID.get(block.typeId);
 
-  if (block.typeId === POTASSIUM_BLOCK_ID) {
+  if (!metal) {
+    return;
+  }
+
+  if (metal.blockId === "chemistry_plus:potassium_metal_block") {
     player.sendMessage(player.name + " 放置了鉀金屬方塊，請小心！");
     player.sendMessage("事件偵測：你放置了鉀金屬方塊");
+    return;
   }
+
+  player.sendMessage(player.name + " 放置了" + metal.name + "金屬方塊，請小心！");
+  player.sendMessage("事件偵測：你放置了" + metal.name + "金屬方塊");
 });
 
 function getItemStackFromEntity(entity) {
@@ -31,7 +84,7 @@ function getItemStackFromEntity(entity) {
   }
 }
 
-function triggerPotassiumWaterExplosion(itemEntity) {
+function triggerAlkaliMetalWaterExplosion(itemEntity, metal) {
   if (itemEntity.hasTag(REACTION_TAG)) {
     return;
   }
@@ -41,7 +94,7 @@ function triggerPotassiumWaterExplosion(itemEntity) {
   const dimension = itemEntity.dimension;
   const location = itemEntity.location;
 
-  world.sendMessage("鉀金屬掉入水中：發生大爆炸！");
+  world.sendMessage(metal.waterMessage);
 
   try {
     itemEntity.remove();
@@ -56,7 +109,7 @@ function triggerPotassiumWaterExplosion(itemEntity) {
         y: location.y,
         z: location.z
       },
-      EXPLOSION_RADIUS,
+      metal.explosionRadius,
       {
         breaksBlocks: false,
         causesFire: false,
@@ -68,7 +121,7 @@ function triggerPotassiumWaterExplosion(itemEntity) {
   }
 }
 
-function checkDroppedPotassiumItems() {
+function checkDroppedAlkaliMetalItems() {
   const dimension = world.getDimension("overworld");
 
   const itemEntities = dimension.getEntities({
@@ -90,7 +143,9 @@ function checkDroppedPotassiumItems() {
       continue;
     }
 
-    if (itemStack.typeId !== POTASSIUM_ITEM_ID) {
+    const metal = METAL_BY_ITEM_ID.get(itemStack.typeId);
+
+    if (!metal) {
       continue;
     }
 
@@ -98,10 +153,10 @@ function checkDroppedPotassiumItems() {
       continue;
     }
 
-    triggerPotassiumWaterExplosion(itemEntity);
+    triggerAlkaliMetalWaterExplosion(itemEntity, metal);
   }
 }
 
 system.runInterval(() => {
-  checkDroppedPotassiumItems();
+  checkDroppedAlkaliMetalItems();
 }, CHECK_INTERVAL_TICKS);
